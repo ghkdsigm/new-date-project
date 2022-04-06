632b961870127564807e37697b7f9a5a43df4890
2022project <ghkdsigm3@gmail.com>
2022-04-06 23:42:17 +0900

    top버튼 위치 수정

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <AppHeader></AppHeader>
     <div class="app-contents">
       <router-view></router-view>
+
+      <a @click="scrollTop" v-show="visible" class="bottom-right"> </a>
     </div>
     <AppFooter></AppFooter>
   </div>
@@ -17,6 +19,34 @@ export default {
     AppHeader,
     AppFooter,
   },
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  methods: {
+    scrollTop() {
+      this.intervalId = setInterval(() => {
+        if (window.pageYOffset === 0) {
+          clearInterval(this.intervalId);
+        }
+        window.scroll(0, window.pageYOffset - 50);
+      }, 2);
+    },
+    scrollListener(e) {
+      this.visible = window.scrollY > 1000;
+    },
+    scrollToElement() {
+      let element = document.getElementById("toEls");
+      element.scrollIntoView({ behavior: "smooth", block: "end" });
+    },
+  },
+  beforeMount() {
+    window.addEventListener("scroll", this.scrollListener);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.scrollListener);
+  },
 };
 </script>
 
@@ -24,5 +54,24 @@ export default {
 @import "@/assets/scss/main.scss";
 .app-contents {
   margin-top: 74px;
+}
+.bottom-right {
+  position: fixed;
+  bottom: 5%;
+  right: 3%;
+  z-index: 9999;
+  background-image: url("~@/assets/images/arrowTop.png");
+  background-size: cover;
+  color: #fff;
+  padding: 30px;
+  border-radius: 55%;
+  align-items: center;
+  font-weight: 500;
+  font-size: 1.2rem;
+  border: 1px solid #1b1b1b;
+  &:hover {
+    border: 1px solid #96f2d7;
+    cursor: pointer;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,6 @@
       <MainTrend />
       <UpcommingList />
     </div>
-    <a @click="scrollTop" v-show="visible" class="bottom-right"> </a>
   </div>
 </template>
 
@@ -28,55 +27,5 @@ export default {
     MainNow,
     UpcommingList,
   },
-  data() {
-    return {
-      visible: false,
-    };
-  },
-  methods: {
-    scrollTop() {
-      this.intervalId = setInterval(() => {
-        if (window.pageYOffset === 0) {
-          clearInterval(this.intervalId);
-        }
-        window.scroll(0, window.pageYOffset - 50);
-      }, 2);
-    },
-    scrollListener(e) {
-      this.visible = window.scrollY > 1000;
-    },
-    scrollToElement() {
-      let element = document.getElementById("toEls");
-      element.scrollIntoView({ behavior: "smooth", block: "end" });
-    },
-  },
-  beforeMount() {
-    window.addEventListener("scroll", this.scrollListener);
-  },
-  beforeDestroy() {
-    window.removeEventListener("scroll", this.scrollListener);
-  },
 };
 </script>
-
-<style lang="scss" scoped>
-.bottom-right {
-  position: fixed;
-  bottom: 5%;
-  right: 3%;
-  z-index: 9999;
-  background-image: url("~@/assets/images/arrowTop.png");
-  background-size: cover;
-  color: #fff;
-  padding: 30px;
-  border-radius: 55%;
-  align-items: center;
-  font-weight: 500;
-  font-size: 1.2rem;
-  border: 1px solid #1b1b1b;
-  &:hover {
-    border: 1px solid #96f2d7;
-    cursor: pointer;
-  }
-}
-</style>
